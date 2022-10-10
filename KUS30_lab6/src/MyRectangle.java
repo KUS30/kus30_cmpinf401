a0894dc02a6fc1df9bc5c4b784490f87e8ca949a
KUS30 <112411586+KUS30@users.noreply.github.com>
2022-10-10 10:05:14 -0400

    Add files via upload

---
+++ KUS30_lab6/src/MyRectangle.java
@@ -0,0 +1,49 @@
+public class MyRectangle {
+	private int startX;
+	private int startY;
+	private int width;
+	private int height;
+	
+	public MyRectangle() {
+		this.startX = 0;
+		this.startY = 0;
+		this.width = 0;
+		this.height = 0;
+	}
+	public MyRectangle(int startX, int startY, int width, int height) {
+		this.startX = startX;
+		this.startY = startY;
+		this.width = width;
+		this.height = height;
+	}
+	public int area() {
+		return this.width * this.height;
+	}
+	public String toString() {
+		String result = "Rectangle properties: " + "\n" + "Starts at (" + this.startX + ", " + this.startY + ") with width of " + this.width + " and height of " + this.height + ".";
+		return result;
+	}
+	public boolean isInside(int X, int Y) {
+		return (X >= startX && X <= (startX + width)) && (Y >= startY && Y <= (startY + height));
+	}
+	public int getX() {
+		return this.startX;
+	}
+	public int getY() {
+		return this.startY;
+	}
+	public int getWidth() {
+		return this.width;
+	}
+	public int getHeight() {
+		return this.height;
+	}
+	public void setSize(int newWidth, int newHeight) {
+		this.width = newWidth;
+		this.height = newHeight; 
+	}
+	public void setPosition(int newX, int newY) {
+		this.startX = newX;
+		this.startY = newY; 
+	}
+}
